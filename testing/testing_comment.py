bbaff58065845c26fcac2ca3dc329470245eac40
Sebastien Iooss <archimist.linux@gmail.com>
2022-04-05 11:57:33 +0200

    add multi-line test

--- testing/testing_comment.py
+++ testing/testing_comment.py
@@ -7,6 +7,7 @@ class TestComment(unittest.TestCase):
     def validate(self, test_cases: list) -> None:
         for test_case in test_cases:
             output = cooklang.parseRecipe(test_case["input"])
+            print(output)
             self.assertEqual(output["steps"][0][0]["value"], test_case["output_steps"])
 
     def test_basic(self) -> None:
@@ -28,3 +29,15 @@ class TestComment(unittest.TestCase):
         ]
 
         self.validate(test_cases)
+
+    def test_multiline(self) -> None:
+        test_cases = [
+            {
+                "input": "meal for 4 people [- scale linearly  \n for people \n but time does not scale-]",
+                "output_steps": "meal for 4 people ",
+            },
+        ]
+        for test_case in test_cases:
+            output = cooklang.parseRecipe(test_case["input"])
+            print(output)
+            self.assertEqual(len(output["steps"]), 1)
